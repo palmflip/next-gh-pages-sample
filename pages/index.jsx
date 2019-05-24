52ce9176164f4657403abab409186a06393f5fdb
Alexander Simonov <palmflip@gmail.com>
2019-05-24 14:09:50 +0300

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+
+const originPage = props =>
+  <div className={props.className}>
+    <p>
+      Это просто пустая страница.
+    </p>
+    <p>
+      Мы можете начать изменять и заполнять компонентами из библиотеки <code>@csssr/core-design</code> эту страницу как вам угодно или создать новую. 
+      Документацию по существующим компонентам можно найти <a href="http://core-design.csssr.ru/" target="_blank" rel="noopener">тут</a>.
+    </p>
+    <p>
+      Для создания своих собственных компонентов воспользуйтесь командой <code>@csssr/core-design core-design create-component</code>.
+    </p>
+  </div>
+
+const Page = styled(originPage)`
+
+  code {
+    background-color: ${props => props.theme.colors.secondary.origin};
+  }
+`
+
+export default Page
